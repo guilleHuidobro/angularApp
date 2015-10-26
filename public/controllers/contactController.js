ed2d7978bb972ca8fc16d991765938cc288d6cdc
guille huidobro <ghuidobro@belatrixsf.com>
2015-10-26 17:32:25 -0300

    add an array

--- public/controllers/contactController.js
+++ public/controllers/contactController.js
@@ -1,9 +1,17 @@
 angular.module('MyApp')
   .controller('ContactController', function($scope,$alert) {
 
-    $scope.contacts = {name: 'misko', email: 'male', phone:'35454'};
+    $scope.contacts = [{name: 'misko', email: 'male', phone:'35454'}];
 
     $scope.saveContact = function(){
+
+      console.log($scope.newcontact.name);
+      console.log($scope.newcontact.email);
+      console.log($scope.newcontact.phone);
+      console.log($scope.newcontact.id);
+
+      
+
               $alert({
                 title: 'save',
                 content: 'You have successfully save.',
