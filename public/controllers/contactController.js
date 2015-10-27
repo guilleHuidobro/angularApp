c4e2023cbd59d5d9896845b445fe91b3d2ecf119
guille huidobro <ghuidobro@belatrixsf.com>
2015-10-27 12:33:50 -0300

    agenda curso angular

--- public/controllers/contactController.js
+++ public/controllers/contactController.js
@@ -1,24 +1,66 @@
 angular.module('MyApp')
   .controller('ContactController', function($scope,$alert) {
 
-    $scope.contacts = [{name: 'misko', email: 'male', phone:'35454'}];
+    $scope.contacts = [];
+    var id = 1;
 
     $scope.saveContact = function(){
+      if($scope.newcontact.id == null){
+        $scope.newcontact.id = id++;
+        $scope.contacts.push($scope.newcontact)
+      }else{
+        angular.forEach($scope.contacts, function (value) {
+        if(value.id == $scope.newcontact.id) {
+          var i = $scope.contacts.indexOf(value);
+          $scope.contacts[i] = $scope.newcontact;
+        }
+                return;
+        });
 
-      console.log($scope.newcontact.name);
-      console.log($scope.newcontact.email);
-      console.log($scope.newcontact.phone);
-      console.log($scope.newcontact.id);
+      }
+      $scope.newcontact = {};
 
-      
+      $alert({
+        title: 'save',
+        content: 'You have successfully save.',
+        animation: 'fadeZoomFadeDown',
+        type: 'material',
+        duration: 3
+      });
 
-              $alert({
-                title: 'save',
-                content: 'You have successfully save.',
-                animation: 'fadeZoomFadeDown',
-                type: 'material',
-                duration: 3
-              });
     };
 
-  });
+    $scope.edit = function(id){
+      angular.forEach($scope.contacts, function (value) {
+        if(value.id == id) {
+          $scope.newcontact = angular.copy(value);
+        }
+        return;
+      });
+      $alert({
+        title: 'edit',
+        content: 'You have successfully edit.',
+        animation: 'fadeZoomFadeDown',
+        type: 'material',
+        duration: 3
+      });
+    };
+
+    $scope.delete = function(id){
+      angular.forEach($scope.contacts, function (value) {
+          if(value.id == id) {
+              var i = $scope.contacts.indexOf(value);
+              $scope.contacts.splice(i,1);
+              $scope.newcontact = {};
+          return;
+          }
+      });
+      $alert({
+        title: 'delete',
+        content: 'You have successfully delete.',
+        animation: 'fadeZoomFadeDown',
+        type: 'material',
+        duration: 3
+      });
+    };
+});
